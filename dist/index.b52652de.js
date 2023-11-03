4c42a1d99b1df0896480128c8e25e91c42f8500b
Evenstarw1 <40696599+Evenstarw1@users.noreply.github.com>
2023-11-03 01:36:52 +0100

    Added new files

---
+++ dist/index.b52652de.js
@@ -0,0 +1,14 @@
+/* Function to open dropdown menu  */ function navBarOnClick() {
+    document.querySelector("div.navbar__dropdrown").style.display = "block";
+    document.querySelector("div.navbar__bars-close").style.display = "block";
+    document.querySelector("div.navbar__bars-open").style.display = "none";
+    document.querySelector("header").style.height = "260px";
+}
+/* Function to close dropdown menu  */ function navBarCloseOnClick() {
+    document.querySelector("div.navbar__dropdrown").style.display = "none";
+    document.querySelector("div.navbar__bars-close").style.display = "none";
+    document.querySelector("div.navbar__bars-open").style.display = "block";
+    document.querySelector("header").style.height = "unset";
+}
+
+//# sourceMappingURL=index.b52652de.js.map
